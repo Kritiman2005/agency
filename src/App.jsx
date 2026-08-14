8987eb3c1af811e06f5edf9b2c98dfe22cfa4d92
Sampraad <sampraaddas2503@gmail.com>
2026-08-14 13:09:11 +0530

    added analytics for vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import { Analytics } from "@vercel/analytics/react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Lenis from "lenis";
 import Home from "./pages/Home";
@@ -44,6 +45,7 @@ function App() {
 					element={<TermsOfServicePage />}
 				/>
 			</Routes>
+			<Analytics />
 		</Router>
 	);
 }
